c2bb1a6060286bcb07c3481bf0c3164431e5fdee
Raul Flores, Jr. MBA "rudy <rudy@redcrowconsulting.com>
2018-03-27 17:05:25 -0500

    Feat: extra features
     - click and drag to paint
    
     - right click and double click to "erase" (paints white)
    
    rudimusmaximus/feature/clickAndDrag 
    v0.1.0-clickAndDrag.1

--- designs.js
+++ designs.js
@@ -3,10 +3,10 @@
  * the current user size values
  */
 function makeGrid() {
-  const numRowsForHeight = $("#inputHeight").val();
-  const numColumnsForWidth = $("#inputWeight").val();
+  const numRowsForHeight = $('#inputHeight').val();
+  const numColumnsForWidth = $('#inputWeight').val();
   // delete any previous table rows
-  $('#pixelCanvas').empty();
+  pCanvas.empty();
 
   //BUILD TABLE
   let rows = '';
@@ -20,24 +20,58 @@ function makeGrid() {
     r += 1;
   } // end while loop
   //APPEND string which becomes table to canvas element adds to the DOM
-  $('#pixelCanvas').append(rows);
+  pCanvas.append(rows);
 }
 /**
- * @description OnReady function that passes a function into the jQuery
- * object to run when the document is ready. Here we add listeners for
+ * @description effectively an OnReady function that passes a function into the jQuery
+ * object which runs when the document is ready. Here we add listeners for
  * canvas and submit events
  */
 $(function() {
   makeGrid();
-  // attaches a delegated event handler
-  $('#pixelCanvas').on("click", "td", function(e) {
-    // get currently selected color
-    let color = $('#colorPicker').val();
-    // paint background if event is on a table cell
-    $(this).css("backgroundColor", color);
-  });
-  $('#sizePicker').submit(function(e) {
-    makeGrid();
-    e.preventDefault();
-  });
-});//end on ready
+}); //end on ready
+
+// ATTACH delegated event handlers
+
+// Handel submit events
+$('#sizePicker').submit(function(e) {
+  makeGrid();
+  e.preventDefault();
+});
+// Set canvas variables
+let pCanvas = $('#pixelCanvas');
+const defaultBlankCanvasColor = "#ffffff";
+
+// SIMULATE ERASERS by restoring white for any table cell in canvas
+// by Doubleclick
+pCanvas.on("dblclick", "td", function(e) {
+  e.preventDefault(); //avoid any other default actions
+  $(this).css("background-color", defaultBlankCanvasColor);
+});
+// by right click
+pCanvas.on("contextmenu", "td", function(e) {
+  e.preventDefault(); //avoid contextmenu in canvas table/grid
+  $(this).css("background-color", defaultBlankCanvasColor);
+});
+
+// ENABLE CLICK AND DRAG
+let mouseDown = false;
+// instead of on click use mousedown so first block is painted when doing a click and DRAG
+// user thinks "click and drag" but really it's mousedown and drag
+pCanvas.on("mousedown", "td", function(e) {
+  e.preventDefault(); //avoid any other default actions
+  mouseDown = true;
+  let color = $("#colorPicker").val();
+  $(this).css("background-color", color);
+});
+pCanvas.on("mouseup", "td", function(e) {
+  e.preventDefault(); //avoid any other default actions
+  mouseDown = false;
+});
+pCanvas.on("mouseover", "td", function(e) {
+  e.preventDefault(); //avoid any other default actions
+  if (mouseDown) {
+    let color = $("#colorPicker").val();
+    $(this).css("background-color", color);
+  }
+});
